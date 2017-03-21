ff1c3a5b937ff6e3fdbcb127f90ad0a4f92afd9f
mlinglhl <minhung.ling@gmail.com>
2017-03-21 10:52:26 -0400

    Initial Commit

---
+++ ScriptMemory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScriptMemory
+//
+//  Created by Minhung Ling on 2017-03-21.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
